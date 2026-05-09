770871f03d3613e0399d6d120ac4ddf9aef23635
kloud <carltonjuguna@gmail.com>
2026-05-09 18:55:18 +0200

    22

--- ikbodymodif.py
+++ ikbodymodif.py
@@ -8,6 +8,11 @@ def IKinBody(Blist, M, T, thetalist0, eomg, ev):
     maxiterations = 20
     Vb = mr.se3ToVec(mr.MatrixLog6(np.dot(mr.TransInv(mr.FKinBody(M, Blist, \
                                                       thetalist)), T)))
+    print(mr.FKinBody(M, Blist,thetalist))
+    print(thetalist)
+    print(Vb)
+    print(np.linalg.norm([Vb[0], Vb[1], Vb[2]]))
+    print(np.linalg.norm([Vb[3], Vb[4], Vb[5]]))
     err = np.linalg.norm([Vb[0], Vb[1], Vb[2]]) > eomg \
           or np.linalg.norm([Vb[3], Vb[4], Vb[5]]) > ev
     while err and i < maxiterations:
@@ -20,11 +25,15 @@ def IKinBody(Blist, M, T, thetalist0, eomg, ev):
                                                        thetalist)), T)))
         err = np.linalg.norm([Vb[0], Vb[1], Vb[2]]) > eomg \
               or np.linalg.norm([Vb[3], Vb[4], Vb[5]]) > ev
-    return (thetalist, not err)
+        print(err)
+        #print(np.linalg.norm([Vb[0], Vb[1], Vb[2]]))
+        #print(np.linalg.norm([Vb[3], Vb[4], Vb[5]]))
+        #print(mr.FKinBody(M, Blist,thetalist))
+    return (thetalist)
 
-W1, W2 = 109, 82
-L1, L2 = 425, 392
-H1, H2 = 89, 95
+W1, W2 = 0.109, 0.082
+L1, L2 = 0.425, 0.392
+H1, H2 = 0.089, 0.095
 
 M = np.array([
     [-1, 0, 0, L1 + L2],
@@ -49,6 +58,8 @@ blist = np.array([
     [0, 0, 1, 0, 0, 0]                     # Articulation 6
 ]).T
 
-thetalist = np.array([3.062079, -1.481344, 0.059104, 0.059775, 0.037360, 0.029888])
+thetalist = np.array([0,-1,1,0,1,0])
 
 print(IKinBody(blist,M,T_sd,thetalist,0.001,0.0001))
+
+-3.69706963,-18.23390307,42.2551894,-24.02125206,-0.55547097,-1.57083577
